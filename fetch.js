791a05f0535cf7a5911c30611861cd12cb18e072
Anwar <anwarjahid449@gmail.com>
2023-02-23 16:08:39 +0600

    fetch api

---
+++ fetch.js
@@ -0,0 +1,6 @@
+function loadData() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
